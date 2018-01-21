3f002143c6788fda7e4c9c3058c724a84c503e61
Marcos Campos <marcam@microsoft.com>
2018-01-21 12:10:25 +0000

    marcos 21 janeiro

--- Projeto Programacao Jan 2018/Projeto Programacao Jan 2018/main.cpp
+++ Projeto Programacao Jan 2018/Projeto Programacao Jan 2018/main.cpp
@@ -1,3 +1,8 @@
+
+//**************************
+// ref 073 como referencia
+//**************************
+
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
@@ -8,11 +13,6 @@
 //#include <WinBase.h>
 using namespace std;
 
-//a inventar cenas dos refs 073 e afins
-//declarações
-
-
-// TESTE DA CATIA
 struct livro
 {
 	long num;
@@ -22,20 +22,46 @@ struct livro
 };
 
 struct livro liv;
-
-//ref 073
-
 FILE *fp;
-
 char ch;
-
 
 //funções
 int listar() {
+	//estudar melhor os REF 74 & 72, pois esta função não está a funcionar bem
+	//exemplo: há neste momento dois registos - 123 e 1234 - se escrevermos 1 ele devolve o primeiro
+	//se escrevermos 1234 ele só devolve o 123
+
+	long salto;
 	system("cls");
 	cout << "######################" << endl;
 	cout << "\n\t\tOpção Listar" << endl;
 	cout << "\n######################" << endl;
+
+	// aceder e ler um registo especifico  
+	// fazer reset do ptr do fich para o inicio
+
+	fp = fopen("dados.dat", "r");
+
+	printf("\n\n\nDigite o numero do registo que pretenda ler: ");
+
+	fflush(stdin);
+	scanf("%ld", &salto);
+
+	// posicionar-se
+
+	fseek(fp, (long)(salto - 1) * sizeof(liv), SEEK_SET);
+
+	// ler o reg localizado 
+	fread(&liv, sizeof(liv), 1, fp);
+
+	// mostrar o reg seleccionado
+	printf("\nISBN: %ld", liv.num);
+	printf("\nNome: %s", liv.nome);
+	printf("\nAutor: %s", liv.autor);
+	printf("\nValor: %4.2f\n\n\n", liv.valor);
+
+	fclose(fp);
+
 	system("PAUSE");
 	return 0;
 };
@@ -44,59 +70,59 @@ int inserir() {
 	system("cls");
 	cout << "######################" << endl;
 	cout << "\n\t\tOpção Inserir" << endl;
-	cout << "\n######################" << endl;
-	
-	
-	// abertura do ficheiro para escrita-append 
+	cout << "\n######################" << endl;	
+	//o programa ao correr mostra as linhas acima e fica a espera de um ENTER
+	//como saltamos a frente e mostramos logo as perguntas?
 
+	
+	//abre ficheiro
 	fp = fopen("dados.dat", "a");
+	
 	//dormir aqui? Sleep(3000);
+	
 	if (fp == NULL)
 	{
 		cout << "ERRO!\nO Ficheiro não foi aberto.\n" << endl;
 
 		cout << "\n\nDigite uma tecla para terminar Programa\n" << endl;
 
-		cout.flush();
-		//fflush(stdin);
+		cout.flush();		
 		ch = getchar();
-
 		exit(0);
 	}
 		
 	if (_getch() != 27)
 		do
-		{
-			// ler os dados de cada registo e a seguir carregá-lo no 
-			// ficheiro binário
-			// repetir até que se digite a tecla ESC
-
-			//system("cls");
-
+		{			
+			// nota - não estamos a validar a introdução. experimentem ver a opção consultar, visto
+			// se inserirem espaços/ENTER ele guarda isso tudo no ficheiro e a consulta/listagem fica "feia"
 			printf("\n\nDigite o ISBN do livro: ");
 			fflush(stdin);
-			scanf("%ld", &liv.num);
+			scanf_s("%ld", &liv.num);
+			printf("\n\n\nISBN: %ld", liv.num);
 
-			printf("\n\nDigite o Titulo do livro: ");
+			// HELP - não consigo que a pergunta "titulo do livro" funcione no visual studio
+			// :( Simplesmente salta para a pergunta seguinte
+			
+			printf("\n\nDigite o Titulo do livro:");
 			fflush(stdin);
-			gets(liv.nome);
+			fgets(liv.nome, 30, stdin);
+			// fgets é o gets mas novo e atualizado
+			printf("\nNome: %s", liv.nome);
 
 			printf("\n\nDigite o Autor do livro: ");
 			fflush(stdin);
-			gets(liv.autor);
+			fgets(liv.autor, 30, stdin);
+			printf("\nAutor: %s", liv.autor);
 
 			printf("\n\nDigite o valor do livro: ");
 			fflush(stdin);
-			scanf("%f", &liv.valor);
-
-			// os args do comando fwrite são:
-			//
-			// o endereço da variável da estrutura, o tamanho da 
-			// estrutura em bytes, o numero de registos a gravar
-			// e o ponteiro associado ao ficheiro
-
+			scanf_s("%f", &liv.valor);
+			printf("\nValor: %4.2f", liv.valor);
+			
 			fwrite(&liv, sizeof(liv), 1, fp);
 
+			// o ESC está a gerar loop no menu inicial é preciso perceber porquê
 			printf("\n\nClique em ESC para sair\n\n");
 			printf("\t ou em outra tecla para novo registo\n\n");
 
@@ -191,10 +217,10 @@ int main() {
 
 		//Marcos a testar - validação do input. o objetivo é só conseguir inserir numeros e não outros chars
 		if (cin.fail()) {
-			system("cls");
+			/*system("cls");
 			cout << "erro";
 			cin.clear();
-			system("pause");
+			system("pause");*/
 		}
 		else {
 
